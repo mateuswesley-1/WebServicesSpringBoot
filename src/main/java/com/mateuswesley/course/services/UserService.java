9c499bb4db36de3a6381943b76becc3c14af50e5
Mateus Wesley <mateuswesley314@gmail.com>
2023-02-13 08:05:46 -0300

    Exception: delete

--- src/main/java/com/mateuswesley/course/services/UserService.java
+++ src/main/java/com/mateuswesley/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mateuswesley.course.entities.User;
 import com.mateuswesley.course.repositories.UserRepository;
+import com.mateuswesley.course.services.exceptions.DataBaseException;
 import com.mateuswesley.course.services.exceptions.ResourceNotFoundException;
 
 // Registra essa classe como um componente (de serviço) que podera // ser usado para injeção de dependencias
@@ -36,7 +39,21 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        
+        try{
+
+            repository.deleteById(id);
+
+        } catch(EmptyResultDataAccessException e) {
+
+            throw new ResourceNotFoundException(id);
+
+        } catch( DataIntegrityViolationException e){
+
+            throw new DataBaseException(e.getMessage());
+
+        }
+
     }
 
     public User update(Long id, User user){
---
+++ src/main/java/com/mateuswesley/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.mateuswesley.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+
+}
